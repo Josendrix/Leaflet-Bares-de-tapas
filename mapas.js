b3fdd1f8f67ecfa66516802a10bf8585c15b1717
Jose Manuel Roco <josendrix@hotmail.com>
2017-10-03 21:15:04 +0200

    Colocado script en head

--- mapas.js
+++ mapas.js
@@ -1,88 +1,89 @@
-﻿// icono de autibuses.
+﻿function leaflet (){
+	// icono de autibuses.
 
-var BusIcon = L.icon({
-    iconUrl: 'bus.png',
+	var BusIcon = L.icon({
+   	 iconUrl: 'bus.png',
 
-    iconSize:[50, 60], // Tamaño del icono.
-});
+   	 iconSize:[50, 60], // Tamaño del icono.
+	});
 
-var map = L.map('map').
-setView([37.174320, -3.598454],
-15);
+	var map = L.map('map').
+		setView([37.174320, -3.598454],
+		15);
 
-var mapLayer = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
-maxZoom: 18, 
-}).addTo(map);
+	var mapLayer = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+		maxZoom: 18, 
+		}).addTo(map);
 
 /*
 map.addLayer(L.tileLayer);
 */
 
-var googleLayer = new L.Google();
-map.addLayer(googleLayer);
+	var googleLayer = new L.Google();
+	map.addLayer(googleLayer);
 
-var googleRoadmapLayer = new L.Google('ROADMAP');
-map.addLayer(googleRoadmapLayer);
+	var googleRoadmapLayer = new L.Google('ROADMAP');
+	map.addLayer(googleRoadmapLayer);
 
 /* Aqui va la capa con los markets*/
 
-var Bar1	= L.marker([37.169413, -3.599356]).bindPopup('<b>Bar-Aliatar</b><br>Precio: 2€'),
-    Bar2	= L.marker([37.179966, -3.600563]).bindPopup('<b>Tacon flamenco</b><br>Precio: 2€'),
-    Bar3	= L.marker([37.174198, -3.598251]).bindPopup('<b>La Blanca Paloma</b><br>No se puede elegir la tapa<br>Tapa escasa<br>Poca variedad de tapas<br>Es más bien restaurante por raciones<br>Precio:2.1€<br>Tapa probada: Berengenas fritas'),
-    Bar4	= L.marker([37.173455, -3.597831]).bindPopup('<b>Las Copas</b><br>No se puede elegir la tapa<br>Tapa escasa<br>Precio: 2.20€<br>Tapa probada: Boquerones en vinagre'),
-    Bar5	= L.marker([37.173204, -3.597239]).bindPopup('<b>La Chopera</b><br>No se pujede elegir la tapa<br>Buena tapa pero de tamaño normalito<br>Precio: 2.10€<br>Tapa probada: Mini rosca de jamón con tomate y aceitunas');
+	var Bar1	= L.marker([37.169413, -3.599356]).bindPopup('<b>Bar-Aliatar</b><br>Precio: 2€'),
+	    Bar2	= L.marker([37.179966, -3.600563]).bindPopup('<b>Tacon flamenco</b><br>Precio: 2€'),
+	    Bar3	= L.marker([37.174198, -3.598251]).bindPopup('<b>La Blanca Paloma</b><br>No se puede elegir la tapa<br>Tapa escasa<br>Poca variedad de tapas<br>Es más bien restaurante por raciones<br>Precio:2.1€<br>Tapa probada: Berengenas fritas'),
+	    Bar4	= L.marker([37.173455, -3.597831]).bindPopup('<b>Las Copas</b><br>No se puede elegir la tapa<br>Tapa escasa<br>Precio: 2.20€<br>Tapa probada: Boquerones en vinagre'),
+	    Bar5	= L.marker([37.173204, -3.597239]).bindPopup('<b>La Chopera</b><br>No se pujede elegir la tapa<br>Buena tapa pero de tamaño normalito<br>Precio: 2.10€<br>Tapa probada: Mini rosca de jamón con tomate y aceitunas');
 
-var Bares = L.layerGroup([Bar1,Bar2,Bar3,Bar4,Bar5]);
+	var Bares = L.layerGroup([Bar1,Bar2,Bar3,Bar4,Bar5]);
 
 // capa de las lineas de autobuses.
-var autobuses =L.marker([37.176448, -3.597781],{icon: BusIcon}).addTo(map).bindPopup('Parada 414: SN1,LAC'),
-	bus1 =L.marker([37.172618, -3.599508],{icon: BusIcon}).addTo(map).bindPopup('Parada 74: SN1,121(nocturno),LAC'),
-	bus2 =L.marker([37.175482, -3.596622],{icon: BusIcon}).addTo(map).bindPopup('Parada 487: C5'),
-	bus3 =L.marker([37.172439, -3.597167],{icon: BusIcon}).addTo(map).bindPopup('Parada 1434: C5'),
-	bus4 =L.marker([37.177051, -3.59522],{icon: BusIcon}).addTo(map).bindPopup('Parada 1105: C1,C2'),
-	bus5 =L.marker([37.173636, -3.595364],{icon: BusIcon}).addTo(map).bindPopup('Parada 1351: C3,C4');
+	var autobuses =L.marker([37.176448, -3.597781],{icon: BusIcon}).addTo(map).bindPopup('Parada 414: SN1,LAC'),
+		bus1 =L.marker([37.172618, -3.599508],{icon: BusIcon}).addTo(map).bindPopup('Parada 74: SN1,121(nocturno),LAC'),
+		bus2 =L.marker([37.175482, -3.596622],{icon: BusIcon}).addTo(map).bindPopup('Parada 487: C5'),
+		bus3 =L.marker([37.172439, -3.597167],{icon: BusIcon}).addTo(map).bindPopup('Parada 1434: C5'),
+		bus4 =L.marker([37.177051, -3.59522],{icon: BusIcon}).addTo(map).bindPopup('Parada 1105: C1,C2'),
+		bus5 =L.marker([37.173636, -3.595364],{icon: BusIcon}).addTo(map).bindPopup('Parada 1351: C3,C4');
 
-var bus = L.layerGroup([autobuses,bus1,bus2,bus3,bus4,bus5]);
+	var bus = L.layerGroup([autobuses,bus1,bus2,bus3,bus4,bus5]);
 
 /*------------------------------------------------*/
 
-var baseMaps = {
-	'Google Roadmap' : googleRoadmapLayer,
-	'Google' : googleLayer,
-	'leaflet' : mapLayer
-};
+	var baseMaps = {
+		'Google Roadmap' : googleRoadmapLayer,
+		'Google' : googleLayer,
+		'leaflet' : mapLayer
+	};
 
-var overlayMaps = {"Bares": Bares, "bus": bus};
+	var overlayMaps = {"Bares": Bares, "bus": bus};
 
 
 
-L.control.layers(baseMaps,overlayMaps).addTo(map);
+	L.control.layers(baseMaps,overlayMaps).addTo(map);
 
-map.dragging.disable(); //no permite mover el mapa
-map.doubleClickZoom.disable();//no zoom doble click
-map.scrollWheelZoom.disable();//no zoom rueda raton
-map.keyboard.disable(); //no movimiento con teclado
-map.removeControl(map.zoomControl); //desabilita los botones para el zoom
+	map.dragging.disable(); //no permite mover el mapa
+	map.doubleClickZoom.disable();//no zoom doble click
+	map.scrollWheelZoom.disable();//no zoom rueda raton
+	map.keyboard.disable(); //no movimiento con teclado
+	map.removeControl(map.zoomControl); //desabilita los botones para el zoom
 
 /*L.control.scale().addTo(map);
 
 
-map.locate({setView: true, maxZoom: 15});
+	map.locate({setView: true, maxZoom: 15});
 
-function onLocationFound(e) {
+	function onLocationFound(e) {
 
-	L.marker(e.latlng).addTo(map);
+		L.marker(e.latlng).addTo(map);
 
-	var	p1 = new L.LatLng(37.188076, -3.614460),
-		p2 = new L.LatLng(37.184042, -3.604847),
-		p3 = new L.LatLng(37.183153, -3.609954),
-		polygonPoints = [p1, p2, p3];
-	var polygon = new L.Polygon(polygonPoints);
-	map.addLayer(polygon);
+		var	p1 = new L.LatLng(37.188076, -3.614460),
+			p2 = new L.LatLng(37.184042, -3.604847),
+			p3 = new L.LatLng(37.183153, -3.609954),
+			polygonPoints = [p1, p2, p3];
+		var polygon = new L.Polygon(polygonPoints);
+		map.addLayer(polygon);
 
-}
+	}
 
-map.on('locationfound', onLocationFound);
-
+	map.on('locationfound', onLocationFound);
 */
+}
